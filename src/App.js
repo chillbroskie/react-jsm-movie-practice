f6d215e0e7469f982d06b3b4330004317e078eb2
chillbroskie <64366196+chillbroskie@users.noreply.github.com>
2022-04-13 16:54:31 -0400

    input and search icon

--- src/App.js
+++ src/App.js
@@ -1,5 +1,35 @@
-function App() {
-  return <div className="App"></div>;
-}
+import React from "react";
+import "./App.css";
+import SearchIcon from "./search.svg";
+import { useEffect } from "react";
+
+const API_URL = "http://www.omdbapi.com?apikey=959dd6ef";
+
+const App = () => {
+  const searchMovies = async (title) => {
+    const response = await fetch(`${API_URL}&s=${title}`);
+    const data = await response.json();
+
+    console.log(data.Search);
+  };
+
+  useEffect(() => {
+    searchMovies("Spiderman");
+  }, []);
+
+  return (
+    <div className="app">
+      <h1>MovieLand</h1>
+      <div className="search">
+        <input
+          placeholder="search for movies"
+          value="Spiderman"
+          onChange={() => {}}
+        />
+        <img src={SearchIcon} />
+      </div>
+    </div>
+  );
+};
 
 export default App;
